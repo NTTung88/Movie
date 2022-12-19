f9c20518ed96bbce80cf6d82150740db21b18c40
Thanh Tung <ngthtung0808@gmail.com>
2022-12-19 22:06:47 +0700

    Building Default layout

---
+++ src/config/routes.js
@@ -0,0 +1,5 @@
+const routes = {
+    home: '/',
+    history: '/history',
+};
+export default routes;
---
+++ src/routes/routes.js
@@ -0,0 +1,11 @@
+import config from '~/config';
+
+import Home from '~/pages/Home';
+import History from '~/pages/History';
+
+const publicRoutes = [
+    { path: config.routes.home, component: Home },
+    { path: config.routes.history, component: History },
+];
+
+export { publicRoutes };

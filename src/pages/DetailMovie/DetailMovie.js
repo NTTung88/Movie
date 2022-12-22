1af2938168d8590897b624b29d50291e5c2e3d71
Thanh Tung <ngthtung0808@gmail.com>
2022-12-22 22:43:46 +0700

    add cmt for movie details and change bach-ground in detail page

--- src/pages/DetailMovie/DetailMovie.js
+++ src/pages/DetailMovie/DetailMovie.js
@@ -6,37 +6,106 @@ const cx = classNames.bind(style);
 
 function DetailMovie() {
     return (
-        <div className={cx('wrapper')}>
-            <div className={cx('section1')}>
-                <p className={cx('title')}>
-                    Avengers: Endgame and the most beautiful greeting for the magnificent epic
-                </p>
-                <div className={cx('sectionLeft')}>
-                    <img
-                        className={cx('img')}
-                        src="https://revelogue.com/wp-content/uploads/2020/01/Poster-chinh-thuc-cho-Endgame-e1578281933829.jpg"
-                        alt="aveee"
-                    />
-                </div>
-                <div className={cx('sectionRight')}>
-                    <p className={cx('titleContent')}>Category:</p>
-                    <p className={cx('titleContent')}>Status:</p>
-                    <p className={cx('titleContent')}>Point:</p>
-                    <p className={cx('titleContent')}>Reissue:</p>
-                    <p className={cx('titleContent')}>Time:</p>
-                </div>
-                <div className={cx('sectionBottom')}>
-                    <p className={cx('content')}>
-                        Avengers: Endgame is a 2019 American superhero film based on the Marvel Comics superhero team
-                        the Avengers. Produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures,
-                        it is the direct sequel to Avengers: Infinity War (2018) and the 22nd film in the Marvel
-                        Cinematic Universe (MCU). Directed by Anthony and Joe Russo and written by Christopher Markus
-                        and Stephen McFeely, the film features an ensemble cast including Robert Downey Jr., Chris
-                        Evans, Mark Ruffalo, Chris Hemsworth, Scarlett Johansson, Jeremy Renner, Don Cheadle, Paul Rudd,
-                        Brie Larson, Karen Gillan, Danai Gurira, Benedict Wong, Jon Favreau, Bradley Cooper, Gwyneth
-                        Paltrow, and Josh Brolin. In the film, the surviving members of the Avengers and their allies
-                        attempt to reverse the destruction caused by Thanos in Infinity War.
+        <div className={cx('bodyDetail')}>
+            <div className={cx('wrapper')}>
+                <div className={cx('section1')}>
+                    <p className={cx('title')}>
+                        Avengers: Endgame and the most beautiful greeting for the magnificent epic
                     </p>
+                    <div className={cx('sectionLeft')}>
+                        <img
+                            className={cx('img')}
+                            src="https://revelogue.com/wp-content/uploads/2020/01/Poster-chinh-thuc-cho-Endgame-e1578281933829.jpg"
+                            alt="aveee"
+                        />
+                    </div>
+                    <div className={cx('sectionRight')}>
+                        <p className={cx('titleContent')}>Category:</p>
+                        <p className={cx('titleContent')}>Status:</p>
+                        <p className={cx('titleContent')}>Point:</p>
+                        <p className={cx('titleContent')}>Reissue:</p>
+                        <p className={cx('titleContent')}>Time:</p>
+                        <button className={cx('btnSR')}>Watching</button>
+                        <button className={cx('btnSR')}>Following</button>
+                    </div>
+                    <div className={cx('sectionMidle')}>
+                        <p className={cx('content')}>
+                            Avengers: Endgame is a 2019 American superhero film based on the Marvel Comics superhero
+                            team the Avengers. Produced by Marvel Studios and distributed by Walt Disney Studios Motion
+                            Pictures, it is the direct sequel to Avengers: Infinity War (2018) and the 22nd film in the
+                            Marvel Cinematic Universe (MCU). Directed by Anthony and Joe Russo and written by
+                            Christopher Markus and Stephen McFeely, the film features an ensemble cast including Robert
+                            Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth, Scarlett Johansson, Jeremy Renner,
+                            Don Cheadle, Paul Rudd, Brie Larson, Karen Gillan, Danai Gurira, Benedict Wong, Jon Favreau,
+                            Bradley Cooper, Gwyneth Paltrow, and Josh Brolin. In the film, the surviving members of the
+                            Avengers and their allies attempt to reverse the destruction caused by Thanos in Infinity
+                            War.
+                        </p>
+                    </div>
+
+                    <div className={cx('sectionBottom')}>
+                        <p className={cx('titleComment')}>Comment (1000)</p>
+                        <div className={cx('contentComment')}>
+                            <img
+                                className={cx('avatar')}
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShoFqZAQmdEqxqn9LxC8W2YG-RXszdpQud5g&usqp=CAU"
+                                alt="chicken"
+                            />
+                            <p className={cx('nickName')}>Tùng Nguyễn</p>
+                            <p className={cx('cmtLine')}>Phim này rất hay.</p>
+                            <p className={cx('timeLine')}>1 phút trước.</p>
+                        </div>
+                        <div className={cx('contentComment')}>
+                            <img
+                                className={cx('avatar')}
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShoFqZAQmdEqxqn9LxC8W2YG-RXszdpQud5g&usqp=CAU"
+                                alt="chicken"
+                            />
+                            <p className={cx('nickName')}>Tùng Nguyễn</p>
+                            <p className={cx('cmtLine')}>Phim này rất hay.</p>
+                            <p className={cx('timeLine')}>1 phút trước.</p>
+                        </div>
+                        <div className={cx('contentComment')}>
+                            <img
+                                className={cx('avatar')}
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShoFqZAQmdEqxqn9LxC8W2YG-RXszdpQud5g&usqp=CAU"
+                                alt="chicken"
+                            />
+                            <p className={cx('nickName')}>Tùng Nguyễn</p>
+                            <p className={cx('cmtLine')}>Phim này rất hay.</p>
+                            <p className={cx('timeLine')}>1 phút trước.</p>
+                        </div>
+                        <div className={cx('contentComment')}>
+                            <img
+                                className={cx('avatar')}
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShoFqZAQmdEqxqn9LxC8W2YG-RXszdpQud5g&usqp=CAU"
+                                alt="chicken"
+                            />
+                            <p className={cx('nickName')}>Tùng Nguyễn</p>
+                            <p className={cx('cmtLine')}>Phim này rất hay.</p>
+                            <p className={cx('timeLine')}>1 phút trước.</p>
+                        </div>
+                        <div className={cx('contentComment')}>
+                            <img
+                                className={cx('avatar')}
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShoFqZAQmdEqxqn9LxC8W2YG-RXszdpQud5g&usqp=CAU"
+                                alt="chicken"
+                            />
+                            <p className={cx('nickName')}>Tùng Nguyễn</p>
+                            <p className={cx('cmtLine')}>Phim này rất hay.</p>
+                            <p className={cx('timeLine')}>1 phút trước.</p>
+                        </div>
+                        <div className={cx('contentComment')}>
+                            <img
+                                className={cx('avatar')}
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShoFqZAQmdEqxqn9LxC8W2YG-RXszdpQud5g&usqp=CAU"
+                                alt="chicken"
+                            />
+                            <p className={cx('nickName')}>Tùng Nguyễn</p>
+                            <p className={cx('cmtLine')}>Phim này rất hay.</p>
+                            <p className={cx('timeLine')}>1 phút trước.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
